952ff9beeddc0944397469909b9310616656ed3c
Steve Konves <stephen.konves@gmail.com>
2021-10-20 07:43:09 -0700

    feat: run all sorbet factories

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {
   defaultFactories,
   ValidatorFactory as TypescriptValidatorFactory,
 } from './typescript/validator-factory';
+import { ValidatorFactory as SorbetValidatorFactory } from './sorbet/validator-factory';
 
 const factories = [
   new ComposedFileFactory(
@@ -14,7 +15,11 @@ const factories = [
     new TypescriptInterfaceFactory(),
     new TypescriptValidatorFactory(defaultFactories),
   ),
-  new SorbetInterfaceFactory(),
+  new ComposedFileFactory(
+    'ruby-sorbet',
+    new SorbetInterfaceFactory(),
+    new SorbetValidatorFactory(),
+  ),
 ];
 
 export const allowedTargets = factories.map((f) => f.target);
